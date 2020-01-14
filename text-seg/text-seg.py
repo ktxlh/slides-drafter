31b313b515d3d35d2bf707b4d85da45f957f79a0
Shang Ling Hsu <shanglinghsu@gmail.com>
2020-01-14 21:45:16 +0800

    Add preprocessing code

--- text-seg/text-seg.py
+++ text-seg/text-seg.py
@@ -23,26 +23,27 @@ from itertools import combinations
 from nltk.tokenize import sent_tokenize
 
 import torch
-from torch.utils.data import (DataLoader, RandomSampler, TensorDataset)
+from torch.utils.data import (DataLoader, RandomSampler, TensorDataset, random_split)
 from transformers import BertForSequenceClassification, BertTokenizer
 
 # Parameters
 max_epochs = 10
-params = {'batch_size': 16, # TODO batch_size was 64 in the example
-          'shuffle': True,
-          'num_workers': 6}
+batch_size = 16 # TODO batch_size was 64 in the example
+max_sent_len = 40
 random.seed(666)
 torch.manual_seed(666)
 json_dir = "/home/shanglinghsu/ml-camp/wiki-vandalism/mini-json" # Should be json
 # mini-json: Subset with only 7822*.json and 7823*.json
-num_par_train = 10
-
-SENT_TEMP = "[CLS] {} [SEP] {}"
-
+num_paragraphs_used = 10 ### TODO
+tokenizer_encode_plus_parameters = {
+    'max_length' : max_sent_len,
+    'pad_to_max_length' : 'right',
+}
 
 # Load model
 model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+PAD_ID = tokenizer.pad_token_id
 """
 ## From pre_trained
 model = model_class.from_pretrained('./directory/to/save/')  # re-load
@@ -68,44 +69,28 @@ for root, dirs, files in os.walk(json_dir):
                     continue # Some tables are weird <1>
                 print(len(sentences))
                 sections.append(sentences)
-        if len(sections) >= num_par_train: ### TODO Use more data later
+        if len(sections) >= num_paragraphs_used: ### TODO Use more data later
             break
 
 print("# of sections loaded:", len(sections))
 
-## Sentences from same section
-same_sec_pairs = []
-for section in sections:
-    for comb in combinations(section, 2):
-        if random.random() < 0.5: ### TODO How many from same/diff sections?
-            same_sec_pairs.append(tokenizer.encode(SENT_TEMP.format(comb[0], comb[1])))
-### TODO 改！這個要先processed再做embedding
-same_sec_input_ids = torch.tensor(same_sec_pairs)
-same_sec_labels = torch.ones([same_sec_input_ids.size()[0],1]) # same number of 1s
+sent_secs, inputs, labels = [],[],[]
+for i in range(len(sections)):
+    sent_secs.extend(zip([i]*len(sections[i]), sections[i]))
+combs = combinations(sent_secs, 2)
+for (l1,s1), (l2,s2) in combs:
+    inputs.append(
+        tokenizer.encode_plus(
+            s1, s2, **tokenizer_encode_plus_parameters))
+    labels.append(int(l1 == l2))
 
-print(same_sec_pairs[0])
-print(same_sec_input_ids[0])
-print(same_sec_labels[0])
+inputs = torch.tensor(inputs)
+labels = torch.tensor(labels)
 
-## Sentences from diff sections
-diff_sec_pairs = []
-for comb in combinations(sections, 2):
-    if random.random < 0.5:
-        sent1 = random.choice(comb[0])
-        sent2 = random.choice(comb[1])
-        diff_sec_pairs.append(tokenizer.encode(SENT_TEMP.format(sent1, sent2))))
-diff_sec_input_ids = torch.tensor(diff_sec_pairs)
-diff_sec_labels = torch.zeros([diff_sec_input_ids.size()[0],1]) # same number of 0s
+data = TensorDataset(inputs, labels)
+train_set, test_set = random_split(data, [int(len(labels)*0.8), int(len(labels)*0.2)])
 
-print(diff_sec_pairs[0])
-print(diff_sec_input_ids[0])
-print(diff_sec_labels[0])
-
-inputs_ids = torch.cat((same_sec_input_ids, diff_sec_input_ids), 0)
-labels = torch.cat((same_sec_labels, diff_sec_labels), 0)
-ds = Dataset(inputs_ids, labels)
-train_set, test_set = torch.utils.data.random_split(ds, [int(num_par_train*0.8), int(num_par_train*0.2)])
-
+dataloader = DataLoader(train_set, sampler=RandomSampler(data), batch_size=batch_size)
 
 # Fine-tune model
 for 
@@ -114,14 +99,14 @@ for
 # Test fine-tuned model
 
 """
-input_ids = torch.tensor([tokenizer.encode("Let's see all hidden-states and attentions on this text")])
+inputs = torch.tensor([tokenizer.encode("Let's see all hidden-states and attentions on this text")])
 labels = None
 
 # If you used to have this line in pytorch-pretrained-bert:
-loss = model(input_ids, labels=labels)
+loss = model(inputs, labels=labels)
 
 # Now just use this line in transformers to extract the loss from the output tuple:
-outputs = model(input_ids, labels=labels)
+outputs = model(inputs, labels=labels)
 loss = outputs[0]
 
 # In transformers you can also have access to the logits:
@@ -129,7 +114,7 @@ loss, logits = outputs[:2]
 
 # And even the attention weights if you configure the model to output them (and other outputs too, see the docstrings and documentation)
 model = BertForSequenceClassification.from_pretrained('bert-base-uncased', output_attentions=True)
-outputs = model(input_ids, labels=labels)
+outputs = model(inputs, labels=labels)
 loss, logits, attentions = outputs
 """
 
