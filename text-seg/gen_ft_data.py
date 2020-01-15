c6b060d961cace5e41b619e04a71226cf7fa2219
Shang Ling Hsu <shanglinghsu@gmail.com>
2020-01-15 21:10:01 +0800

    Fix typo

--- text-seg/gen_ft_data.py
+++ text-seg/gen_ft_data.py
@@ -11,7 +11,7 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 docs = traverse_json_dir(json_dir, return_docs=False)
-shuffle(docs))
+shuffle(docs)
 l_tr = int(float(len(docs))*0.8)
 
 with open(os.path.join(output_dir,"train.txt"),'w') as fout:
