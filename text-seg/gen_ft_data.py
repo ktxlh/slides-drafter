4f2e11202a440e994721d717a405ab9878f18adc
Shang Ling Hsu <shanglinghsu@gmail.com>
2020-01-15 21:09:23 +0800

    Randomize train test split

--- text-seg/gen_ft_data.py
+++ text-seg/gen_ft_data.py
@@ -1,16 +1,17 @@
 import os
-
+from random import shuffle
 from nltk.tokenize import sent_tokenize
 
 from utils import traverse_json_dir
 
-json_dir = "/home/shanglinghsu/ml-camp/wiki-vandalism/mini-json" # Should be json
+json_dir = "/home/shanglinghsu/ml-camp/wiki-vandalism/json" # Should be json
 output_dir = json_dir+"-raw"
 print("output_dir:", output_dir)
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 docs = traverse_json_dir(json_dir, return_docs=False)
+shuffle(docs))
 l_tr = int(float(len(docs))*0.8)
 
 with open(os.path.join(output_dir,"train.txt"),'w') as fout:
