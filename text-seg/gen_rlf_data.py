ac822eec9f8169eefb2c8c25b7f5628337af5b0e
Shang Ling Hsu <shanglinghsu@gmail.com>
2020-01-15 08:04:34 +0800

    commented out tensorboard

--- text-seg/gen_rlf_data.py
+++ text-seg/gen_rlf_data.py
@@ -11,7 +11,7 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 sections = traverse_json_dir(json_dir, toke_to_sent=False, limit_paragraphs=0)
-l_tr = float(len(sections))*0.8
+l_tr = int(float(len(sections))*0.8)
 
 with open(os.path.join(output_dir,"train.txt"),'w') as fout:
     fout.write("\n".join(sections[:l_tr])+"\n")
--- text-seg/run_lm_finetuning.py
+++ text-seg/run_lm_finetuning.py
@@ -62,10 +62,10 @@ from transformers import (
 )
 
 
-try:
-    from torch.utils.tensorboard import SummaryWriter
-except ImportError:
-    from tensorboardX import SummaryWriter
+#try:
+#    from torch.utils.tensorboard import SummaryWriter
+#except ImportError:
+#    from tensorboardX import SummaryWriter
 
 
 logger = logging.getLogger(__name__)
@@ -193,8 +193,8 @@ def mask_tokens(inputs: torch.Tensor, tokenizer: PreTrainedTokenizer, args) -> T
 
 def train(args, train_dataset, model, tokenizer):
     """ Train the model """
-    if args.local_rank in [-1, 0]:
-        tb_writer = SummaryWriter()
+    #if args.local_rank in [-1, 0]:
+    #    tb_writer = SummaryWriter()
 
     args.train_batch_size = args.per_gpu_train_batch_size * max(1, args.n_gpu)
     train_sampler = RandomSampler(train_dataset) if args.local_rank == -1 else DistributedSampler(train_dataset)
@@ -332,10 +332,10 @@ def train(args, train_dataset, model, tokenizer):
                         args.local_rank == -1 and args.evaluate_during_training
                     ):  # Only evaluate when single GPU otherwise metrics may not average well
                         results = evaluate(args, model, tokenizer)
-                        for key, value in results.items():
-                            tb_writer.add_scalar("eval_{}".format(key), value, global_step)
-                    tb_writer.add_scalar("lr", scheduler.get_lr()[0], global_step)
-                    tb_writer.add_scalar("loss", (tr_loss - logging_loss) / args.logging_steps, global_step)
+                        #for key, value in results.items():
+                        #    tb_writer.add_scalar("eval_{}".format(key), value, global_step)
+                    #tb_writer.add_scalar("lr", scheduler.get_lr()[0], global_step)
+                    #tb_writer.add_scalar("loss", (tr_loss - logging_loss) / args.logging_steps, global_step)
                     logging_loss = tr_loss
 
                 if args.local_rank in [-1, 0] and args.save_steps > 0 and global_step % args.save_steps == 0:
@@ -366,8 +366,8 @@ def train(args, train_dataset, model, tokenizer):
             train_iterator.close()
             break
 
-    if args.local_rank in [-1, 0]:
-        tb_writer.close()
+    #if args.local_rank in [-1, 0]:
+    #    tb_writer.close()
 
     return global_step, tr_loss / global_step
 
